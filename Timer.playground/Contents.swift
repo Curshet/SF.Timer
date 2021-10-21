8238b87df400ad09cbd7461dfcd5214a3133c4d7
Daniel Martines <curshet@mail.ru>
2021-10-22 02:23:45 +0300

    Refactoring project file
    - Add extension for class Table
    - Add formatting of the project code
    - Change .init() location in class Timer

--- Timer.playground/Contents.swift
+++ Timer.playground/Contents.swift
@@ -1,93 +1,140 @@
-import Cocoa
 import Foundation
 
-class Table: CustomStringConvertible {
-    
-    private var timers: [Timer] = []
-    
-    private func updateTimers() {
-        var validTimers: [Timer] = []
-        for timer in timers {
-            if !timer.isFinished() {
-                validTimers.append(timer)
-            }
-        timers = validTimers
-    }
+
+
+class Table {
+	
+	private var timers: [Timer] = []
+	
+	private func updateTimers() {
+		
+		var validTimers: [Timer] = []
+		
+		
+		for timer in timers {
+			
+			if !timer.isFinished() {
+				
+				validTimers.append(timer)
+				
+			}
+			
+			timers = validTimers
+			
+		}
+		
+	}
 }
-    
-    func updateTable() {
-        updateTimers()
-        timers = timers.sorted { $0.remainTime()! > $1.remainTime()! }
-    }
-    
-    var description: String {
-        return "\(timers)"
-    }
-    
-    func addTimer(timer: Timer) {
-        timers.append(timer)
-    }
-    
+
+
+//MARK: CustomStringConvertible Protocol
+
+extension Table: CustomStringConvertible {
+	
+	var description: String {
+		
+		"\(timers)"
+		
+	}
+	
+	
+	func updateTable() {
+		
+		updateTimers()
+		timers = timers.sorted { $0.remainTime()! > $1.remainTime()! }
+		
+	}
+	
+	
+	func addTimer(timer: Timer) {
+		
+		timers.append(timer)
+		
+	}
+	
 }
+
 
 
 class Timer {
-    
-    var name: String
-    var countDown: Int
-    
-    private var startTime: Int?
-    private var pauseStatus = false
-    private var pauseTime: Int?
-    private var pausePeriod = 0
-    
-    func start() {
-        guard pauseTime == nil else { return }
-        startTime = Int(Date().timeIntervalSince1970)
-    }
-    
-    func isFinished() -> Bool {
-        guard startTime == nil else { return true }
-        
-        let currentTime = Int(Date().timeIntervalSince1970)
-        if pauseTime != nil {
-             return false
-        } else {
-            return (currentTime - startTime!) - pausePeriod > countDown
-        }
-    }
-
-    func remainTime() -> Int? {
-        guard startTime != nil else { return nil }
-        
-        let currentTime = Int(Date().timeIntervalSince1970)
-        return currentTime - startTime!
-    }
-
-    func pause() {
-        pauseTime = Int(Date().timeIntervalSince1970)
-    }
-    
-    func resume() {
-        guard pauseTime != nil else { return }
-        
-        let currentTime = Int(Date().timeIntervalSince1970)
-        let diff = currentTime - pauseTime!
-        pausePeriod += diff
-        pauseTime = nil
-        pauseStatus = false
-    }
-    
-    init(name: String, countDown: Int) {
-        self.name = name
-        self.countDown = countDown
-    }
-    
+	
+	var name: String
+	var countDown: Int
+	
+	private var startTime: Int?
+	private var pauseStatus = false
+	private var pauseTime: Int?
+	private var pausePeriod = 0
+	
+	
+	init(name: String, countDown: Int) {
+		
+		self.name = name
+		self.countDown = countDown
+		
+	}
+	
+	
+	func start() {
+		
+		guard pauseTime == nil else { return }
+		startTime = Int(Date().timeIntervalSince1970)
+		
+	}
+	
+	
+	func isFinished() -> Bool {
+		
+		guard startTime == nil else { return true }
+		
+		let currentTime = Int(Date().timeIntervalSince1970)
+		
+		if pauseTime != nil {
+			
+			return false
+			
+		} else {
+			
+			return (currentTime - startTime!) - pausePeriod > countDown
+			
+		}
+		
+	}
+	
+	
+	func remainTime() -> Int? {
+		
+		guard startTime != nil else { return nil }
+		
+		let currentTime = Int(Date().timeIntervalSince1970)
+		return currentTime - startTime!
+		
+	}
+	
+	
+	func pause() {
+		
+		pauseTime = Int(Date().timeIntervalSince1970)
+		
+	}
+	
+	
+	func resume() {
+		
+		guard pauseTime != nil else { return }
+		let currentTime = Int(Date().timeIntervalSince1970)
+		let diff = currentTime - pauseTime!
+		pausePeriod += diff
+		pauseTime = nil
+		pauseStatus = false
+		
+	}
+	
 }
 
 
 //var tableOne: Table = .init()
-//var timerOne: Timer = .init(name: "Timer1", countDown: 999)
+//var timerOne: Timer = .init(name: "Circle", countDown: 999)
 //tableOne.addTimer(timer: timerOne)
 //print(tableOne)
 //timerOne.start()
